050db128ca3e81ab8259672828c6435c808340ec
Dave Hulihan <dhulihan@gmail.com>
2016-02-20 14:50:03 -0700

    gd improvements

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -12,7 +12,8 @@ alias gplom='gpl origin master'
 alias gplod='gpl origin dev'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push'
-alias gd='git diff'
+# pretty diff with colors instead of prefixed +|- (better for yanking)
+alias gd='git diff --color | sed -E "s/^([^-+ ]*)[-+ ]/\\1/" | less -r'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
