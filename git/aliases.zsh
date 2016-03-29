442167dad00f3efdfddca7e74103a61379198d52
dhulihan <dhulihan@sorensonmedia.com>
2016-03-29 13:23:33 -0600

    sh-common

---
+++ git/aliases.zsh
@@ -0,0 +1,8 @@
+# Use `hub` as our git wrapper:
+#   http://defunkt.github.com/hub/
+hub_path=$(which hub)
+if (( $+commands[hub] ))
+then
+  alias git=$hub_path
+fi
+
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,31 +1,5 @@
 alias reload!='. ~/.zshrc'
 
-# Generic process finder 
-alias p="ps aux | grep"
-
-# grep
-alias grin='grep -rin'
-
-alias ll='ls -pliah'
-alias find-recently-modified="print -rl **/*(.0m)"
-
-# Move to /tmp instead of hard delete
-alias rmv="mv -t /tmp"
-
-# Enhanced WHOIS lookups
-#alias whois="whois -h whois-servers.net"
-
-# Misc
-alias youtube-dl-mp3="youtube-dl --extract-audio --audio-format mp3 --title --no-mtime"
-
-# WAN IP
-alias lan-ip="ipconfig getifaddr en0"
-alias wan-ip="dig +short myip.opendns.com @resolver1.opendns.com"
-
-# View HTTP traffic
-alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
-alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
-
 # Serve current directory on port (default 3000)
 # Usage: serve 80
 function serve {
