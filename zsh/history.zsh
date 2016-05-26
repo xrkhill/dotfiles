44ce92cec356456e5d6cf0bea7e3ae5930e05635
dhulihan <dhulihan@sorensonmedia.com>
2016-05-26 16:17:12 -0600

    local history

---
+++ zsh/history.zsh
@@ -0,0 +1,15 @@
+# Make up/dn traverse local history instead of global
+bindkey "^[P" up-line-or-local-history
+bindkey "^[N" down-line-or-local-history
+
+up-line-or-local-history() {
+    zle set-local-history 1
+    zle up-line-or-history
+    zle set-local-history 0
+}
+zle -N up-line-or-local-history
+down-line-or-local-history() {
+    zle set-local-history 1
+    zle down-line-or-history
+    zle set-local-history 0
+}
