cb5efb473397219759b14781f23dc01b854c6e0a
dhulihan <dhulihan@sorensonmedia.com>
2017-03-02 14:10:27 -0700

    go-test-color

--- go/aliases.zsh
+++ go/aliases.zsh
@@ -1,1 +1,2 @@
 alias g="cd $GOPATH/src/github.com/`git config --get github.user`"
+alias go-test-color='go test -v ./... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/'''
