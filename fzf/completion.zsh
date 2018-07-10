9690c99dd7e1556088a89506f0f27296084df3df
dhulihan <dhulihan@sorensonmedia.com>
2018-07-10 10:51:46 -0600

    zshrc cleanup, remove user-specific paths

---
+++ fzf/completion.zsh
@@ -0,0 +1,3 @@
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "$HOME/shell/completion.zsh" 2> /dev/null
---
+++ fzf/path.zsh
@@ -0,0 +1,4 @@
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="$PATH:$HOME/.fzf/bin"
+fi
+
