6e1143a2e50a02e7ea20aa17c22c59869e84cb0d
Richard Hill <112472+xrkhill@users.noreply.github.com>
2019-01-17 15:57:32 -0700

    Updated vim and brew install

--- brew/install.sh
+++ brew/install.sh
@@ -4,51 +4,31 @@ rbenv ruby-build rbenv-vars\
 ssh-copy-id \
 wget \
 wdiff \
-macvim \
 terminal-notifier \
 cmake \
 direnv \
 ctags \ # exuberant
 ack \
-tmate \
-openssl \
-libyaml \
 libffi \
-curl --with-openssl \
 jq \
 watch \
 markdown \
-cowsay \
-fortune \
 coreutils \
-elixir \
-riak \
-logstash \
 tree \
 kubernetes-cli \
 kubernetes-helm \
-graphviz \
-gimp \
-terraform \
 editorconfig \
 ncdu \
-python \
 pg_top \
-mpv \
 direnv \
 fzf \
 kubernetes-cli \
 iftop \
-mysql \
 dep \
-tidy-html5 \
 protobuf \
-erlang
 
 # finish installing fzf
 $(brew --prefix)/opt/fzf/install
-
-brew cask install java
 
 brew tap burntsushi/ripgrep https://github.com/BurntSushi/ripgrep.git
 brew install burntsushi/ripgrep/ripgrep-bin
@@ -63,15 +43,10 @@ brew install kubeval
 
 brew cask install minikube
 
-brew install neovim/neovim/neovim
-
-brew tap wata727/tflint
-brew install tflint
-
 say "brew is done"
 
 brew install macvim --with-override-system-vim
-brew link curl --force
+#brew link curl --force
 brew linkapps
 
 # Sierra
