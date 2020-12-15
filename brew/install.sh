226f6b9be7059d61e9d81cf1edf25999e9e22226
Rich Hill <rich.hill@digicert.com>
2020-12-15 09:44:34 -0700

    Copy .dotfiles/bin from upstream

--- brew/install.sh
+++ brew/install.sh
@@ -1,54 +1,53 @@
-brew install \
-go \
-rbenv ruby-build rbenv-vars \
-ssh-copy-id \
-wget \
-wdiff \
-terminal-notifier \
-cmake \
-direnv \
-ctags \
-ack \
-libffi \
-jq \
-watch \
-markdown \
-coreutils \
-tree \
-kubernetes-cli \
-kubernetes-helm \
-editorconfig \
-ncdu \
-pg_top \
-direnv \
-fzf \
-iftop \
-dep \
-protobuf \
-pwgen
+set -e
 
-# finish installing fzf
+brew install ack
+brew install coreutils
+brew install direnv
+brew install editorconfig
+brew install git
+brew install go
+brew install jq
+brew install mosh
+brew install mpv
+brew install pwgen
+brew install rbenv ruby-build rbenv-vars
+brew install ripgrep
+brew install ssh-copy-id
+brew install terminal-notifier
+brew install tmux
+brew install tree
+brew install watch
+brew install wdiff
+brew install wget
+brew install yq
+
+# installing fzf
+brew install fzf
 $(brew --prefix)/opt/fzf/install
 
-brew tap burntsushi/ripgrep https://github.com/BurntSushi/ripgrep.git
-brew install burntsushi/ripgrep/ripgrep-bin
+# install universal-ctags, which has golang support
+brew install --HEAD universal-ctags/universal-ctags/universal-ctags
 
-# Hack fonts
-brew tap caskroom/fonts
+# install age encryption tool
+brew tap filippo.io/age https://filippo.io/age
+brew install age
+
+# install hack fonts
+brew tap homebrew/cask-fonts
 brew cask install font-hack-nerd-font
+brew cask install font-source-code-pro
+
+brew cask install ngrok
+
+# kubernetes tools
+brew install k9s
+brew install kubectx
+brew install kubernetes-cli
+brew install kubernetes-helm
 
 # kubeval
-brew tap garethr/kubeval
+brew tap instrumenta/instrumenta
 brew install kubeval
 
-brew cask install minikube
-
+set +e
 say "brew is done"
-
-brew install macvim --with-override-system-vim
-#brew link curl --force
-
-#brew linkapps # deprecated?
-
-# Sierra
-brew install reattach-to-user-namespace
